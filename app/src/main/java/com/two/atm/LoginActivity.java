2beba40f534584805350a27e89a4abf232528956
tt8871720 <tt8871720@gmail.com>
2018-11-19 21:59:12 +0800

    storage userid

--- app/src/main/java/com/two/atm/LoginActivity.java
+++ app/src/main/java/com/two/atm/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.two.atm;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,25 +10,37 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText edUserid;
+    private EditText edPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        EditText edUserid = findViewById(R.id.ed_userid);
-        edUserid.setText(getSharedPreferences("atm" , MODE_PRIVATE).getString("USERID" , ""));
+        EditText edusername = findViewById(R.id.ed_userid);
+        String uid = getSharedPreferences("Atm" , MODE_PRIVATE)
+                .getString("USERNAME" , " ");//" "為空字串
+        edusername.setText(uid);
+
+//        EditText edUserid = findViewById(R.id.ed_userid);
+//        edUserid.setText(getSharedPreferences("atm" , MODE_PRIVATE).getString("USERID" , ""));
     }
     public void login(View view){
                  //變數名稱                                            //id
-        EditText edUserid = findViewById(R.id.ed_userid);
-        EditText edPassword = findViewById(R.id.ed_password);
-        String uid = edUserid.getText().toString();
-        String pw = edPassword.getText().toString();
-        if(uid.equals("apple") && pw.equals("123")){
-            Toast.makeText(this , "登入成功" , Toast.LENGTH_LONG).show();
-            getSharedPreferences("USERID" , MODE_PRIVATE)
-               .edit()
-                    .putString("USERID" , uid)
+        edUserid = findViewById(R.id.ed_userid);
+        edPassword = findViewById(R.id.ed_password);
+      String uid = edUserid.getText().toString();
+      String psw = edPassword.getText().toString();
+        if(uid.equals("apple") && psw.equals("123")){
+            //方法一
+//            SharedPreferences settings = getSharedPreferences("Atm" , MODE_PRIVATE);
+//            settings.edit().putString("USERNAME" , uid).apply();
+            //方法二
+            getSharedPreferences("Atm" , MODE_PRIVATE)
+                    .edit()
+                    .putString("USERNAME" , uid )
                     .apply();
+            Toast.makeText(this, "登入成功" , Toast.LENGTH_LONG ).show();
             setResult(RESULT_OK);
             finish();
         }else{//登入失敗
--- app/src/main/java/com/two/atm/MainActivity.java
+++ app/src/main/java/com/two/atm/MainActivity.java
@@ -4,31 +4,35 @@ import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int RC_LOGIN = 100;
-    boolean logon = false;//預設已經登入了
+    private static final int RC_LOGIN = 1;
+    boolean logon = false; //預設已經登入了
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     if(!logon){//若還沒登入將跳到LoginActivity
-        Intent intent = new Intent(this , LoginActivity.class);
-startActivityForResult(intent , RC_LOGIN);
-    }
-    }
-
+       Intent intent = new Intent(this , LoginActivity.class);
+       startActivityForResult(intent , RC_LOGIN);
+  }
+}
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-if(requestCode == RC_LOGIN){//成功登入,收到回傳值RC_LOGIN(100)
-    if(resultCode != RESULT_OK){}//但不是因為帳密正確而成功所以退出
-    finish();}
-    else{
-    Intent nickname = new Intent(this , NicknameActivity.class );
-    startActivity(nickname);
-}
-
+        if(requestCode == RC_LOGIN){
+            if(resultCode == RESULT_OK){
+            }else {
+                finish();
+            }
+        }
     }
 }
+//    else{
+//    Intent nickname = new Intent(this , NicknameActivity.class );
+//    startActivity(nickname);
+//}
+
+
+
